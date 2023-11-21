d628a9b640d2789b4b020350f290ce6c709b6e90
tkgshn <takagishunsuke1129@gmail.com>
2023-11-21 11:17:08 +0900

    Herokuの認証情報が定期的に変更される場合でも問題ないようについkeないように追記

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    DATABASE_URL: 'postgres://ksbypbyyyvcixb:cb1b9a4b0dcb12211674a4a879cad6d362e8d8b4155e274739f933c44f74b275@ec2-44-213-228-107.compute-1.amazonaws.com:5432/d5g25pt80nb9l3',
     NEXT_PUBLIC_VERCEL_URL: 'http://localhost:3000',
   },
 }
 
-module.exports = nextConfig
+
+// Heroku Postgressの認証情報（DATABASE_URL）は時間経過で変化するので、変わり次第`heroku config:get DATABASE_URL -a pluralqfworkshop > .env.local`で更新しなければいけない
+// 上記コマンドを実行すると、.env.localにDATABASE_URLが書き込まれる
+// ので、この関数では、.env.localが存在する場合は、その内容を読み込んで、nextConfig.env.DATABASE_URLに設定している
+const envLocalPath = path.join(__dirname, '.env.local');
+if (fs.existsSync(envLocalPath)) {
+  const databaseUrl = fs.readFileSync(envLocalPath, 'utf-8').trim();
+  nextConfig.env.DATABASE_URL = databaseUrl;
+}
+
+module.exports = nextConfig;
